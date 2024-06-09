0b6a8640f2c67d5526ad0c64179cd643ae76b121
Gautheron Olivia <oxtramag@gmail.com>
2024-06-09 13:50:31 +0200

    :sparkles:add:feature random traits

---
+++ src/Utils/GenerateSimsData.tsx
@@ -0,0 +1,13 @@
+//IMPORT JSON FRO; TRAIT.JSON
+// incomtability
+
+// FUNC QUI VA GENERER UN TRAIT SIMS
+
+// 1 RANDOM SIMStRAIT 3x via loop et les stocker
+//CONST traits = []
+
+// verifier que il n existe pas deja,
+
+// verifie que les traits precedent ne sont pas incomtabile 
+
+// si c etsle cas re randomize sinon push
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,67 @@
-import './Home.css';
+import { useState } from "react";
+import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
+import Data from "../../services/data";
+import { DataColor } from "../../services/interface";
+import { DataTrait } from "../../services/interface";
 
 function Home() {
-    return (
-        <>
-        <Navbar />
-        <h1>Je Suis un titleeeeeeee</h1>
-        </>
-    )
+  const data = new Data();
+  const dataColor: DataColor[] = data.getDataColor();
+  const dataTraits: DataTrait[] = data.GetDataTrait();
+
+  const [selectedColor, setSelectedColor] = useState<DataColor | null>(null);
+  const [selectedTraits, setSelectedTraits] = useState<DataTrait[]>([]);
+
+  const selectRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * dataColor.length);
+    setSelectedColor(dataColor[randomIndex]);
+  };
+
+  const selectRandomTraits = () => {
+    const shuffled = dataTraits.sort(() => 0.5 - Math.random());
+    setSelectedTraits(shuffled.slice(0, 3));
+  };
+
+  return (
+    <>
+      <Navbar />
+      <h1>Randomizer votre sims :</h1>
+      <div className="color_container">
+        <button className="color_button" onClick={selectRandomColor}>
+          Générer couleur aléatoire
+        </button>
+        {selectedColor && (
+          <div className="color_random">
+            
+            <p className="color_random_name">{selectedColor.color}</p>
+            
+            <div className="color_random_round"
+              style={{
+                backgroundColor: selectedColor.style,
+              }}
+            ></div>
+          </div>
+        )}
+      </div>
+
+      <div className="trait_container">
+        <button className="ctrait_button" onClick={selectRandomTraits}>
+          Générer des trait aléatoires
+        </button>
+        {selectedTraits.length > 0 && (
+          <div className="traits_random">
+            {selectedTraits.map((trait, index) => (
+              <div key={index} className="trait_random">
+                <p className="trait_random_name">{trait.name}</p>
+                <img src={trait.image} alt={trait.name} className="trait_random_image" />
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default Home;
