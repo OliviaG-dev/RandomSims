509a7c600b5c91d7b29b7f924d819ecc4f96df70
Gautheron Olivia <oxtramag@gmail.com>
2025-06-04 12:18:55 +0200

    :bug: fix:bug deploy

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar";
 import Data from "../../services/data";
